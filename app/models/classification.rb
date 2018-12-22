436e604270b5c8eabf0af026b35657998ead9625
mrjak13 <justinkenney13@gmail.com>
2018-12-22 11:08:38 -0800

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    order(:id)
+  end
+
+  def self.longest
+    
+    
+  end
 end
